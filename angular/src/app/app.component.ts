7cfdfdf3955bb5d74e84503243b80fc789632a68
Yoann Breton <yoann4997@hotmail.fr>
2022-02-04 19:37:54 +0100

    Add angular example controls

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular';
+  generateRandomArray() {
+    const arrayLength = 20;
+    return Array.from({ length: arrayLength }, () =>
+      Math.floor(Math.random() * 100)
+    );
+  }
+
+  width = 200;
+  height = 50;
+  lineColor = '#2220a4';
+  bgColor = '#191919';
+  serie = this.generateRandomArray();
 }
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
